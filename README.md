7db5c9df9efb42abb3782e1a02bc46a1c0ef7578
Carlos Andres Sanchez Zorro <csanchezzorro@gmail.com>
2024-09-26 19:15:28 -0500

    Primera versión del proyecto

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # app_environment
+La app tiene el objetivo de gráficar un conjunto de datos relacionado a los anuncios de vehiculos 
---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+
+vehicle_data = pd.read_csv('vehicles_us.csv')
+
+# hist_button = st.button('Construir histograma')  # crear un botón
+
+# if hist_button:  # al hacer clic en el botón
+#     # escribir un mensaje
+#     st.write(
+#         'Creación de un histograma para el conjunto de datos de anuncios de venta de coches'})
+
+
+hist_button = st.button('Construir histograma')  # crear un botón
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(vehicle_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+# disp_button = st.button('Construir gráfico de dispersión')  # crear un botón
+
+# if disp_button:  # al hacer clic en el botón
+#     # escribir un mensaje
+#     st.write(
+#         'Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches'})
+
+disp_button = st.button('Construir gráfico de dispersión')  # crear un botón
+
+if disp_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un gráfico de dispersión
+    fig = px.scatter(vehicle_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
